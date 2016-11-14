3cf6b87cbe710a647dd56f93cb0c08afb0b1d908
Aaron Collier <acollier@calstate.edu>
2016-11-14 10:25:01 -0800

    Adding initial files.

---
+++ app/models/solr_document.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+class SolrDocument
+  include Blacklight::Solr::Document
+  include Blacklight::Gallery::OpenseadragonSolrDocument
+
+  # Adds CurationConcerns behaviors to the SolrDocument.
+  include CurationConcerns::SolrDocumentBehavior
+  # Adds Sufia behaviors to the SolrDocument.
+  include Sufia::SolrDocumentBehavior
+
+
+
+  # self.unique_key = 'id'
+
+  # Email uses the semantic field mappings below to generate the body of an email.
+  SolrDocument.use_extension(Blacklight::Document::Email)
+
+  # SMS uses the semantic field mappings below to generate the body of an SMS email.
+  SolrDocument.use_extension(Blacklight::Document::Sms)
+
+  # DublinCore uses the semantic field mappings below to assemble an OAI-compliant Dublin Core document
+  # Semantic mappings of solr stored fields. Fields may be multi or
+  # single valued. See Blacklight::Document::SemanticFields#field_semantics
+  # and Blacklight::Document::SemanticFields#to_semantic_values
+  # Recommendation: Use field names from Dublin Core
+  use_extension(Blacklight::Document::DublinCore)
+
+  # Do content negotiation for AF models.
+
+  use_extension( Hydra::ContentNegotiation )
+
+  def sponsorship
+    self[Solrizer.solr_name('sponsorship')]
+  end
+
+  def committeemember
+    self[Solrizer.solr_name('committeemember')]
+  end
+end
---
+++ app/models/thesis.rb
@@ -0,0 +1,35 @@
+class Thesis < ActiveFedora::Base
+  include ::CurationConcerns::WorkBehavior
+  include ::CurationConcerns::BasicMetadata
+  include Sufia::WorkBehavior
+  self.human_readable_type = 'Thesis'
+  self.human_readable_type = 'Thesis'
+  # Change this to restrict which works can be added as a child.
+  # self.valid_child_concerns = []
+  validates :title, presence: { message: 'Your thesis must have a title.' }
+
+  property :genre, predicate: ::RDF::Vocab::DC.type do |index|
+    index.as :stored_searchable
+  end
+
+  property :coverage, predicate: ::RDF::Vocab::DC.coverage do |index|
+    index.as :stored_searchable
+  end
+
+  property :sponsorship, predicate: ::RDF::Vocab::DC.description do |index|
+    index.as :stored_searchable, :facetable
+  end
+
+  property :alt_title, predicate: ::RDF::Vocab::DC.alternative do |index|
+    index.as :stored_searchable
+  end
+
+  property :committeemember, predicate: ::RDF::Vocab::DC.contributor do |index|
+    index.as :stored_searchable, :facetable
+  end
+
+  
+  property :provenance, predicate: ::RDF::Vocab::DC.provenance
+  property :date_accessioned, predicate: ::RDF::Vocab::DC.dateAccepted, multiple: false
+
+end
